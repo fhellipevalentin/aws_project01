ecbc89269b779e0088c7bb7b2e275aa5d9a498ee
Fhellipe Valentin <fhellipereis@outlook.com>
2022-07-14 07:46:59 -0300

    primeiros endpoints REST

--- src/main/java/io/github/fhellipevalentin/aws_project01/AwsProject01Application.java
+++ src/main/java/io/github/fhellipevalentin/aws_project01/AwsProject01Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AwsProject01Application {
+public class  AwsProject01Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AwsProject01Application.class, args);
